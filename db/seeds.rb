9cc212aa3ebc61121a83618ae76d02ec5d4ecfa4
Alexandre VALENTIN <alexandre.valentin42@gmail.com>
2024-12-17 11:17:41 +0100

    Ajout de Tailwind + nouvelle seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,15 +4,14 @@ puts "On efface les jeux"
 Game.delete_all
 # https://api.rawg.io/api/games/3498?key=73f9f9c4d88144b2b79b0eabe9b7e9ca
 api_key = ENV['API_RAWG']
-url1 = "https://api.rawg.io/api/games/3498?key=#{api_key}"
-response = HTTParty.get(url1)
+url = "https://api.rawg.io/api/games?key=#{api_key}"
+response = HTTParty.get(url)
 data = JSON.parse(response.body)
-puts data["name"]
-puts data["released"]
-puts data["background_image"]
-puts data["rating"]
-puts data["genres"][0]["name"]
+all_data = data["results"]
+all_data.each do |game|
+  Game.create(name: game["name"], released: game["released"], background_image: game["background_image"], rating: game["rating"], genre: game["genres"][0]["name"])
+end
 
-Game.create(name: data["name"], released: data["released"], background_image: data["background_image"], rating: data["rating"], genre: data["genres"][0]["name"])
+
 
 puts "Finis"
